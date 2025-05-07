923cf6faa9670e0ee0b250576b852ed6cfacb8a7
Sikandar Chauhan <sikandarc989@gmail.com>
2025-05-07 09:40:49 +0530

    final commit

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -4,6 +4,7 @@ import { upload } from "../middlewares/multer.middlewares.js";
 import { processImport } from "../controllers/import.controllers.js";
 import { importFileValidator } from "../validators/importfile/import.validator.js";
 import { validate } from "../validators/validate.js";
+import Company from "../models/company.model.js";
 
 const router = Router();
 
@@ -11,4 +12,13 @@ router
   .route("/import-xl-csv")
   .post(upload.single("file"), importFileValidator, validate, processImport);
 
+
+  router.get("/companies", async (req, res) => {
+    try {
+      const companies = await Company.find({});
+      res.json(companies);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch companies" });
+    }
+  });
 export default router;
