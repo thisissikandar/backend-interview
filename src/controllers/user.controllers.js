4eca8aa11671541b0697bf45fd89d4073c075655
Sikandar Chauhan <sikandarc989@gmail.com>
2025-08-26 12:45:00 +0530

    dumb

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -156,7 +156,7 @@ const logOutUser = asyncHandler(async (req, res) => {
     secure: NODE_ENV === "production",
     sameSite: NODE_ENV === "production" ? "none" : "lax",
     path: "/",
-    domain: ".onrender.com"
+    domain: "backend-interview-i07d.onrender.com"
   };
   return res
     .status(200)
