cbbb6b25d17bc9e689d671927e22a5fd0ad34255
Sikandar Chauhan <sikandarc989@gmail.com>
2025-08-26 18:01:16 +0530

    fix commit

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -118,7 +118,6 @@ const loginUser = asyncHandler(async (req, res) => {
     httpOnly: true,
     secure: NODE_ENV === "production",
     sameSite: NODE_ENV === "production" ? "none" : "lax",
-    domain: NODE_ENV === "production" ? ".onrender.com" : undefined,
   };
 
   return res
@@ -155,8 +154,6 @@ const logOutUser = asyncHandler(async (req, res) => {
     httpOnly: true,
     secure: NODE_ENV === "production",
     sameSite: NODE_ENV === "production" ? "none" : "lax",
-    domain: NODE_ENV === "production" ? ".onrender.com" : undefined,
-
   };
   return res
     .status(200)
